fc8cff23ba014228160f797c4b4ac3fb7a063cbb
Bartosz222 <62373647+Bartosz222@users.noreply.github.com>
2022-02-22 18:12:35 +0100

    1

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import LoginForm from './components/LoginForm';
+function App() {
+  const adminUser= {
+    email: "admin@admin.com",
+    password: "admin123",
+  }
+  const [user,setUser] = useState({name:"",email:""});
+  const [error,setError] = useState("");
+
+  const Login = details =>{
+    console.log(details);
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("LOGIN");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+      setError("");
+    }
+    else{
+      console.log("wrong");
+      setError("Details dont match!!!");
+    }
+  }
+  const Logout = ()=>{
+    console.log("Logout");
+    setUser({name:"",email:""});
+  }
+  return (
+    <div className="App">
+      {(user.email !="") ? (
+        <div className='welcome'>
+           <h2>Welocome, <span>{user.name}</span></h2>
+           <button onClick={Logout}>Logout</button>
+        </div>
+      ):(
+        <LoginForm Login={Login} error={error}/>
+      )}
+    </div>
+  );
+}
+
+export default App;
